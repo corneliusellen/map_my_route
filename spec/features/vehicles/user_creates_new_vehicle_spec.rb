0e7ab0d844e9e969c452778acd3f73f15dfd00c0
Ellen Cornelius <ecorneli@iwu.edu>
2018-02-10 09:37:58 -0700

    Initial commit. Add test gems. Add vehicles routes. Create feature test for vehicle create page.

---
+++ spec/features/vehicles/user_creates_new_vehicle_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe "user can create new vehicle" do
+  describe "user links from root page" do
+    context "user fills in car info and clicks submit" do
+      scenario "user sees index page with new vehicle" do
+        visit new_vehicle_path
+
+        fill_in('Nickname', with: 'Shauna')
+        fill_in('Make', with: 'Chevy')
+        fill_in('Model', with: 'Bolt')
+        fill_in('Year', with: 2017)
+        fill_in('MPG', with: 45)
+        fill_in('Fuel Type', with: 'electric')
+        click_button('Create')
+
+        expect(current_path).to eq(vehicles_path)
+        expect(page).to have_content("Shauna")
+        expect(page).to have_content("electric")
+        expect(Vehicle.all.count).to eq(1)
+      end
+    end
+  end
+end
