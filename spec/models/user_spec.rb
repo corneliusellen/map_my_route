2aa5e808e6b8eb87f2879bb68931a309c4a57094
Ellen Cornelius <ecorneli@iwu.edu>
2018-02-14 07:20:10 -0700

    Write model spec for validations on user.

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe User do
+  describe "validates" do
+    it "prescence of username" do
+    user = User.new(password: "123")
+
+    expect(user).to_not be_valid
+    end
+
+    it "prescence of password" do
+      user = User.new(username: "doug")
+
+      expect(user).to_not be_valid
+    end
+
+    it "uniqueness of username" do
+      original = User.new(username: "doug", password: "lol")
+      copy_cat = User.new(username: "doug", password: "123")
+
+      expect(copy_cat).to_not be_valid
+    end
+  end
+end
