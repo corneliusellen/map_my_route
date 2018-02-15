0c4d5c59796e985d050de7dd5f325f78799a4373
Ellen Cornelius <ecorneli@iwu.edu>
2018-02-15 07:33:10 -0700

    Create fuel types in Application Helper. Add dropdown of fuel types to search pages. Edit index search pages to include user value.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+  def fuel_types
+    {
+      'Electric' => 'ELEC',
+      'Ethanol' => 'E85',
+      'Biodiesel' => 'BD',
+      'Compresses Natural Gas' => 'CNG',
+      'Liquified Natural Gas' => 'LNG',
+      'Hydrogen' => 'HY',
+      'Propane' => 'LPG'
+    }
+  end
 end
